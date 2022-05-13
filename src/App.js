bc877c10316179a644f840cd45f2f32bed23923f
Brayan Rios Valencia <brayan.rios.valencia@gmail.com>
2022-05-13 18:04:58 -0500

    Priemea publicación

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Alert, Card, DatePicker } from 'antd';
+
+var date_test = '';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class='container'>
+        <h1>Components Ant Design</h1>
+        <span>Programacion III</span>
+        <div>
+          <Alert message='Success Text' type='success' />
+          <Alert message='Info Text' type='info' />
+          <Alert message='Warning Text' type='warning' />
+          <Alert message='Error Text' type='error' />
+        </div>
+        <div>
+          <label>Selecciona una fecha</label>
+          <DatePicker onChange={date_test}></DatePicker>
+        </div>
+        <div>
+          <Card size='small' title='Tarjeta de presentación' extra={<a href='#'>Ver más</a>} style={{width: 300}}>
+            <p>Brayan Rios Valencia</p>
+            <p>Ingenieria de sistemas</p>
+            <p>
+              <small>brayansriosv@autonoma.edu.co</small>
+            </p>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 }
